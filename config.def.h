bb1d03f5c1ab646e2e95c754c60b86ad5f8abfb1
piksel_ <3000piksel@gmail.com>
2022-08-13 08:44:04 +0200

    personal config

--- config.def.h
+++ config.def.h
@@ -1,52 +1,41 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 5;        /* gaps between windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int gappx     = 15;        /* gaps between windows */
+static const unsigned int snap      = 0;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int vertpad            = 10;       /* vertical padding of bar */
-static const int sidepad            = 10;       /* horizontal padding of bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const int vertpad            = 15;       /* vertical padding of bar */
+static const int sidepad            = 15;       /* horizontal padding of bar */
+static const char *fonts[]          = { "MesloLGS NF:style=Regular:pixelsize=10:antialias=true:autohint=true" };
+static const char dmenufont[]       = "MesloLGS NF:style=Regular:pixelsize=10:antialias=true:autohint=true";
+static const char col_norm[]        = "#b3b3b3";
+static const char col_sel[]         = "#ffffff";
+static const char col_bg[]          = "#101214";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-	[SchemeStatus]={ col_cyan, col_gray1,  NULL  },
+        /*               fg        bg      border */
+        [SchemeNorm] = { col_norm, col_bg, col_bg },
+        [SchemeSel]  = { col_sel,  col_bg, col_bg },
+        [SchemeStatus]={ col_sel,  col_bg, NULL   },
 };
 
 
 /* status bar */
 static const Block blocks[] = {
-	/* fg     command				interval	signal */
-	{ col_gray3, "sb-clock",			20,		1},
-	{ col_gray1, "sb-disk",				9000,		2},
-	{ col_gray2, "sb-battery",			10,		3},
-	{ col_gray3, "sb-internet",			10,		4},
-	{ col_cyan, "sb-mailbox",			0,		5},
-	{ "#000001", "sb-moonphase",			0,		6},
-	{ "#1F0077", "sb-forecast",			0,		7},
-	{ "#000077", "sb-volume",			0,		8},
-	{ "#F77000", "sb-pacpackages",			0,		9},
-	{ "#177000", "sb-sync",				0,		10},
-//	{ col_gray1, "sb-mpc",				0,		26},
-	{ col_gray2, "sb-music",			0,		11},
-//	{ col_gray3, "sb-tasks",			10,		12},
-	{ col_gray4, "sb-notes",			0,		13},
-	{ col_cyan, "echo '';cat /tmp/recordingicon",	0,		14},
+	/* fg      command				                                interval	signal */
+	{ col_sel, "date +%R:%S",										1,			1},
+	{ col_sel, "date +%d.%m.%y",									60,			2},
+	{ col_sel, "/home/philip/Documents/dev/scripts/getTemp.sh",		60,			3},
+	{ col_sel, "/home/philip/Documents/dev/scripts/getVolume.sh",	1,			4},
+	{ col_sel, "cat /sys/class/net/eth0/operstate",					10,			5},
+	//{ col_sel, "echo''",											42,			42},
 };
 
 /* inverse the order of the blocks, comment to disable */
 #define INVERSED	1
 /* delimeter between blocks commands. NULL character ('\0') means no delimeter. */
-static char delimiter[] = " ";
+static char delimiter[] = " | ";
 /* max number of character that one block command can output */
 #define CMDLENGTH	50
 
@@ -65,25 +54,24 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ NULL,       NULL,       NULL,       0,            0,           -1 },
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.65; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{ "tile",     tile },    /* first entry is default */
+	{ "float",    NULL },    /* no layout function means floating behavior */
+	{ "monocle",  monocle },
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -95,37 +83,51 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *dmenucmd[]    = { "/home/philip/Documents/dev/scripts/spotlight.sh", NULL };
+static const char *termcmd[]     = { "st", NULL };
+static const char *browsercmd[]  = { "chromium", NULL };
+static const char *toriumcmd[]   = { "/home/philip/Documents/dev/scripts/torium.sh", NULL };
+static const char *lfcmd[]       = { "st", "-e", "lf", NULL };
+static const char *voldowncmd[]  = { "/home/philip/Documents/dev/scripts/volumecontrol.sh", "-r", NULL };
+static const char *volupcmd[]    = { "/home/philip/Documents/dev/scripts/volumecontrol.sh", "-i", NULL };
+static const char *nvimcmd[]     = { "st", "-e", "nvim", NULL };
+static const char *m3morycmd[]   = { "/home/philip/Documents/dev/scripts/m3mory.sh", NULL };
+static const char *spotifycmd[]  = { "spotify", NULL };
+static const char *charcmd[]     = { "/home/philip/Documents/dev/scripts/germanchar.sh", NULL };
+static const char *lockcmd[]     = { "xsecurelock & sleep 5 && sudo zzz", NULL };
+static const char *passcmd[]     = { "passmenu", "-c", "-l", "5", NULL };
 
 static Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
-	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
+    /* modifier                     key            function        argument */
+    { MODKEY,                       XK_space,      spawn,          {.v = dmenucmd } },
+    { MODKEY,                       XK_Return,     zoom,           {0} },
+	{ MODKEY,                       XK_semicolon,  spawn,          {.v = termcmd } },
+    { MODKEY,                       XK_d,          spawn,          {.v = browsercmd } },
+    { MODKEY|ShiftMask,             XK_d,          spawn,          {.v = toriumcmd } },
+    { MODKEY,                       XK_f,          spawn,          {.v = lfcmd } },
+    { MODKEY,                       XK_c,          spawn,          {.v = voldowncmd } },
+    { MODKEY,                       XK_v,          spawn,          {.v = volupcmd } },
+    { MODKEY,                       XK_g,          spawn,          {.v = nvimcmd } },
+    { MODKEY|ShiftMask,             XK_g,          spawn,          {.v = m3morycmd } },
+    { MODKEY,                       XK_s,          spawn,          {.v = spotifycmd } },
+    { MODKEY,                       XK_o,          spawn,          {.v = lockcmd } },
+    { MODKEY,                       XK_apostrophe, spawn,          {.v = charcmd } },
+    { MODKEY,                       XK_a,          spawn,          {.v = passcmd } },
+    { MODKEY,                       XK_r,          togglebar,      {0} },
+    { MODKEY,                       XK_j,          focusstack,     {.i = +1 } },
+    { MODKEY,                       XK_k,          focusstack,     {.i = -1 } },
+    { MODKEY,                       XK_h,          setmfact,       {.f = -0.05} },
+    { MODKEY,                       XK_l,          setmfact,       {.f = +0.05} },
+    { MODKEY,                       XK_w,          killclient,     {0} },
+    { MODKEY,                       XK_y,          setlayout,      {.v = &layouts[0]} },
+    { MODKEY,                       XK_u,          setlayout,      {.v = &layouts[1]} },
+    { MODKEY,                       XK_i,          setlayout,      {.v = &layouts[2]} },
+    { MODKEY,                       XK_p,          togglefloating, {0} },
+    { MODKEY,                       XK_0,          view,           {.ui = ~0 } },
+    { MODKEY,                       XK_m,          focusmon,       {.i = -1 } },
+    { MODKEY,                       XK_comma,      focusmon,       {.i = +1 } },
+    { MODKEY|ShiftMask,             XK_m,          tagmon,         {.i = -1 } },
+    { MODKEY|ShiftMask,             XK_comma,      tagmon,         {.i = +1 } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -161,4 +163,3 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
